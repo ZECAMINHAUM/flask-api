e0c0fbc31ff4d062e129e0a871931dafc01a105a
sacuL174 <ls4388387@gmail.com>
2019-09-15 21:04:27 -0300

    [feature + fix] adding cors and return status code

--- app.py
+++ app.py
@@ -8,34 +8,34 @@ from flask_jwt_extended import (
     get_jwt_identity,
     get_raw_jwt
 )
+from flask_cors import CORS
 import bcrypt
 
 
 
-#URI Do Banco de dados 
-SQLALCHEMY_DATABASE_URI = "mysql://{username}:{password}@{hostname}/{databasename}".format(
-    username = "cold",
-    password = "legen174",
-    hostname = "localhost",
-    databasename = "flasktest",
+#URI Do Banco de dados
+SQLALCHEMY_DATABASE_URI = "mysql+mysqldb://{username}:{password}@{hostname}/{databasename}".format(
+    username = "username",
+    password = "password",
+    hostname = "hostname",
+    databasename = "databasename",
 )
-
-def is_password_valid(self, password):
-    return bcrypt.checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
-
 
 #iniciando app
 app = Flask(__name__)
+CORS(app)
+
+#Banco de dados config 
+app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+app.config['SQLALCHEMY_POOL_RECYCLE'] = 299
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 #jwt config
 app.config['JWT_SECRET_KEY'] = 'secret'
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access']
 
-#Banco de dados config 
-app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 280
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -81,26 +81,26 @@ def register():
         newUser = User(nome = data['nome'], senha=senha)
         db.session.add(newUser)
         db.session.commit()
-        return jsonify({ 'success': True })
+        return jsonify({ 'success': True }), 201
 
-    return jsonify({ 'success': False, 'errors': { 'nome': 'Usuário já cadastrado' } })
+    return jsonify({ 'success': False, 'errors': { 'nome': 'Usuário já cadastrado' } }), 400
 
 
 #Rota para login
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.form
+    data = request.json
     users = User.query.filter_by(nome=data['nome']).first()
     if users is None:
-        return jsonify({ 'success': False, 'errors': { 'nome': 'Nome ou senha incorreto' }})
+        return jsonify({ 'success': False, 'errors': { 'nome': 'Nome ou senha incorreto' }}), 400
 
     senha = str(users.senha).encode('utf-8')
     cmpsenha = str(data['senha']).encode('utf-8')
     
     if bcrypt.checkpw(cmpsenha, senha):
         token = create_access_token(users.nome)
-        return jsonify({ 'success': True, 'token': token })
-    return jsonify({ 'success': False, 'errors': { 'nome': 'Nome ou senha incorreto' }  })
+        return jsonify({ 'success': True, 'token': token }), 200
+    return jsonify({ 'success': False, 'errors': { 'nome': 'Nome ou senha incorreto' }  }), 400
 
 #Rota para logout 
 @app.route('/logout', methods=['GET'])
@@ -108,7 +108,7 @@ def login():
 def logout():
     jti = get_raw_jwt()['jti']
     blacklist.add(jti)
-    return jsonify({ 'success': True })
+    return jsonify({ 'success': True }), 200
 
 #Rota para obter todos os Nomes
 @app.route('/items', methods=['GET'])
@@ -118,41 +118,41 @@ def index():
     items_schema = UserSchema(many=True)
 
 
-    return jsonify({ 'success': True, 'items': items_schema.dump(items) })
+    return jsonify({ 'success': True, 'items': items_schema.dump(items) }), 200
 
 
 #Rota para cadastrar
 @app.route('/item/adicionar', methods=['POST'])
 @jwt_required
 def create():
-    data = request.form
+    data = request.json
     items = Item.query.filter_by(nome=data['nome']).first()
     if items is None:
         item = Item(nome = data['nome'])
         db.session.add(item)
         db.session.commit()
 
-        return jsonify({ 'success': True })
+        return jsonify({ 'success': True }), 201
     else:
-        return jsonify({ 'success': False, 'errors': { 'Nome': 'Nome Já Cadastrado' } })
+        return jsonify({ 'success': False, 'errors': { 'Nome': 'Nome Já Cadastrado' } }), 400
 
 
 @app.route('/item/atualizar/<id>', methods=['POST'])
 @jwt_required
 def update(id):
-    data = request.form
+    data = request.json
     print(data['nome'])
     if data['nome'] is None:
-        return jsonify({ 'success': False, 'errors': { 'nome': 'O nome não pode ser nulo' }})
+        return jsonify({ 'success': False, 'errors': { 'nome': 'O nome não pode ser nulo' }}), 400
     else: 
         item = Item.query.filter_by(id=id).first()
         if item is None:
-            return jsonify({ 'success': False, 'errors': { 'item': 'Item não encontrado!'}})
+            return jsonify({ 'success': False, 'errors': { 'item': 'Item não encontrado!'}}), 400
         else:  
             item.nome = data['nome']
             db.session.commit()
 
-        return jsonify({ 'success': True })
+        return jsonify({ 'success': True }), 200
 
 
 @app.route('/item/deletar/<id>', methods=['GET'])
@@ -160,15 +160,13 @@ def update(id):
 def delete(id):
     item = Item.query.filter_by(id=id).first()
     if item is None:
-        return jsonify({ 'success': False, 'errors': { 'item': 'Item não encontrado!'}})
+        return jsonify({ 'success': False, 'errors': { 'item': 'Item não encontrado!'}}), 400
     else:
         db.session.delete(item)
         db.session.commit()
     
-    return jsonify({ 'success': True })
+    return jsonify({ 'success': True }), 200
 
 
-if __name__ == "__main__":
-    db.create_all()
-    app.run(debug=True)
+app.run(debug=True)
 
